171f3ca62420732716888c6130957ff35e32aa8e
doreendilip <88268792+doreendilip@users.noreply.github.com>
2022-03-15 09:00:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
+// finding a series of Armstrong numbers upto n numbers
+
+
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int num, lastDigit, digits, sum, i, end;
+
+
+    printf("Enter the value of the nth number: ");
+    scanf("%d", &end);
+
+    printf("Armstrong numbers between 1 to %d are: \n", end);
+
+    for(i=1; i<=end; i++)
+    {
+        sum = 0;
+
+        num = i;
+
+        digits = (int) log10(num) + 1;
+
+
+        while(num > 0)
+        {
+            lastDigit = num % 10;
+            sum = sum + ceil(pow(lastDigit, digits));
+            num = num / 10;
+        }
+
+        if(i == sum)
+        {
+            printf("%d, ", i);
+        }
+
+    }
+
     return 0;
 }
